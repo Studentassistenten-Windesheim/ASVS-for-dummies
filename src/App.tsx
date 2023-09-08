537ccd66dac34214776e0345c9d3500d207da661
krijn <grimmekrijn@gmail.com>
2023-09-08 12:26:11 +0200

    Fixed App.tsx logic after rewrite

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import './App.css';
 import ASVSListFilter from "./components/ASVSListFilter";
 import ASVSItem from "./model/ASVSItem";
@@ -33,7 +33,25 @@ const App = () => {
         fetchData();
     }, []);
 
-    const setASVSItemState = () => {
+
+    const setChapterCheck = (chapterName: string) => {
+        // Find the chapter that was clicked
+        const chapter = chapters.find((c: ASVSChapter) => c.name === chapterName);
+        if (chapter) {
+            chapter.checked = !chapter.checked;
+            // Force a new array to be created so that React will re-render the component
+            setChapters([...chapters]);
+        }
+    }
+
+    const setLevelCheck = (levelName: string) => {
+        setLevels((prevState) => ({
+            ...prevState,
+            [levelName]: !prevState[levelName]
+        }));
+    }
+
+    const filteredASVSItems = useCallback(() => {
         const checkedChapters = chapters
             .filter((c: ASVSChapter) => c.checked)
             .map((c: ASVSChapter) => c.name);
@@ -42,49 +60,27 @@ const App = () => {
             levels.level2 ||
             levels.level3;
 
-        let newArr: ASVSItem[] = [...asvsItems]
-            .map(i => {
-                i.show = true;
-                return i;
-            })
-            .map((i) => {
-                if (levelFilter) {
-                    let shouldShow = false;
-
-                    if (levels.level1 && i.level1 !== '') shouldShow = true;
-                    if (levels.level2 && i.level2 !== '') shouldShow = true;
-                    if (levels.level3 && i.level3 !== '') shouldShow = true;
-                    i.show = shouldShow
-                }
-                return i;
-            })
-            .map((i) => {
-                if (
-                    checkedChapters.length > 0 &&
-                    checkedChapters.indexOf(i.chapter_name) === -1
-                ) {
-                    i.show = false;
-                }
-                return i;
-            })
-
-        setAsvsItems(newArr);
-    }
-
-    const setChapterCheck = (chapterName: string) => {
-        const newArr: ASVSChapter[] = [...chapters];
-        const chapter: ASVSChapter | undefined = newArr.find((c: ASVSChapter) => c.name === chapterName);
-        if (chapter) chapter.checked = !chapter?.checked;
-        setChapters(newArr);
-        setASVSItemState();
-    }
-
-    const setLevelCheck = (levelName: string) => {
-        const newObj: { [key: string]: boolean } = {...levels};
-        newObj[levelName] = !newObj[levelName];
-        setLevels(newObj);
-        setASVSItemState();
-    }
+        return asvsItems.map((i: ASVSItem) => {
+            i.show = true;
+            if (levelFilter) {
+                let shouldShow = false;
+                
+                if (levels.level1 && i.level1 !== '') shouldShow = true;
+                if (levels.level2 && i.level2 !== '') shouldShow = true;
+                if (levels.level3 && i.level3 !== '') shouldShow = true;
+                i.show = shouldShow
+            }
+            return i;
+        }).map((i: ASVSItem) => {
+            if (
+                checkedChapters.length > 0 &&
+                checkedChapters.indexOf(i.chapter_name) === -1
+            ) {
+                i.show = false;
+            }
+            return i;
+        });
+    }, [chapters, levels, asvsItems]);
 
     return (
         <div className="App">
@@ -111,7 +107,7 @@ const App = () => {
                                 chapters={chapters}
                                 setChapterCheck={(c: string) => setChapterCheck(c)}
                                 setLevelCheck={(c: string) => setLevelCheck(c)}/>
-                <ASVSList items={asvsItems}></ASVSList>
+                <ASVSList items={filteredASVSItems()}></ASVSList>
 
             </div>
         </div>
