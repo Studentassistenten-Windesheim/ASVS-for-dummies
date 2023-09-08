ee6fe74c005ee0760685163f6c057c610cdfa5ad
krijn <grimmekrijn@gmail.com>
2023-09-08 11:35:58 +0200

    Rewrite App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import ASVSListFilter from "./components/ASVSListFilter";
 import ASVSItem from "./model/ASVSItem";
@@ -6,49 +6,43 @@ import {asvsListItemsAPI} from "./api/ASVSApi";
 import ASVSChapter from "./model/ASVSChapter";
 import ASVSList from "./components/ASVSList";
 
-class App extends React.Component<any, any> {
+const App = () => {
+    const [asvsItems, setAsvsItems] = useState<ASVSItem[]>([]);
+    const [chapters, setChapters] = useState<ASVSChapter[]>([]);
+    const [levels, setLevels] = useState<{ [key: string]: boolean }>({
+        level1: false,
+        level2: false,
+        level3: false
+    });
 
-    constructor(props: any) {
-        super(props);
-        this.state = {
-            asvsItems: [],
-            chapters: [],
-            levels: []
-        }
-    }
+    useEffect(() => {
+        const fetchData = async () => {
+            const items = await asvsListItemsAPI();
+            const chapters: ASVSChapter[] =
+                Array.from(items.reduce((p: any, c: ASVSItem) => {
+                    p.add(c.chapter_name);
+                    return p;
+                }, new Set())).map((c: any) => {
+                    return new ASVSChapter(c)
+                });
 
-    async componentDidMount() {
-        const items = await asvsListItemsAPI();
-        const chapters: ASVSChapter[] =
-            Array.from(items.reduce((p: any, c: ASVSItem) => {
-                p.add(c.chapter_name);
-                return p;
-            }, new Set())).map((c: any) => {
-                return new ASVSChapter(c)
-            });
+            setAsvsItems(items);
+            setChapters(chapters);
+        };
 
-        this.setState({
-            asvsItems: items,
-            chapters: chapters,
-            levels: {
-                level1: false,
-                level2: false,
-                level3: false
-            }
-        });
-    }
+        fetchData();
+    }, []);
 
-    setASVSItemState() {
-
-        const checkedChapters = this.state.chapters
+    const setASVSItemState = () => {
+        const checkedChapters = chapters
             .filter((c: ASVSChapter) => c.checked)
             .map((c: ASVSChapter) => c.name);
         const levelFilter: boolean =
-            this.state.levels.level1 ||
-            this.state.levels.level2 ||
-            this.state.levels.level3;
+            levels.level1 ||
+            levels.level2 ||
+            levels.level3;
 
-        let newArr: ASVSItem[] = [...this.state.asvsItems]
+        let newArr: ASVSItem[] = [...asvsItems]
             .map(i => {
                 i.show = true;
                 return i;
@@ -57,9 +51,9 @@ class App extends React.Component<any, any> {
                 if (levelFilter) {
                     let shouldShow = false;
 
-                    if (this.state.levels.level1 && i.level1 !== '') shouldShow = true;
-                    if (this.state.levels.level2 && i.level2 !== '') shouldShow = true;
-                    if (this.state.levels.level3 && i.level3 !== '') shouldShow = true;
+                    if (levels.level1 && i.level1 !== '') shouldShow = true;
+                    if (levels.level2 && i.level2 !== '') shouldShow = true;
+                    if (levels.level3 && i.level3 !== '') shouldShow = true;
                     i.show = shouldShow
                 }
                 return i;
@@ -74,61 +68,54 @@ class App extends React.Component<any, any> {
                 return i;
             })
 
-
-        this.setState({asvsItems: newArr})
-
+        setAsvsItems(newArr);
     }
 
-    setChapterCheck(chapterName: string) {
-        const newArr: ASVSChapter[] = [...this.state.chapters];
+    const setChapterCheck = (chapterName: string) => {
+        const newArr: ASVSChapter[] = [...chapters];
         const chapter: ASVSChapter | undefined = newArr.find((c: ASVSChapter) => c.name === chapterName);
         if (chapter) chapter.checked = !chapter?.checked;
-        this.setState({chapters: newArr}, () => {
-            this.setASVSItemState();
-        })
+        setChapters(newArr);
+        setASVSItemState();
     }
 
-    setLevelCheck(levelName: string) {
-        const newObj: { [key: string]: boolean } = {...this.state.levels};
+    const setLevelCheck = (levelName: string) => {
+        const newObj: { [key: string]: boolean } = {...levels};
         newObj[levelName] = !newObj[levelName];
-        this.setState({levels: newObj}, () => {
-            this.setASVSItemState();
-        });
+        setLevels(newObj);
+        setASVSItemState();
     }
 
-    render() {
-        return (
-            <div className="App">
-                <header className="App-header">
+    return (
+        <div className="App">
+            <header className="App-header">
 
-                </header>
-                <div className="container">
-                    <h1>ASVS for Dummies <small>(ASVS 4.0)</small></h1>
-                    <p><strong>tips:</strong></p>
-                    <ol>
-                        <li>
-                        Gebruik de <a rel="noreferrer" target="_blank" href="https://owasp.org/www-project-secure-coding-practices-quick-reference-guide/">
-                        quick reference guide</a>, daar wordt per requirement zoveel mogelijk naar verwezen.
-                        </li>
-                        <li>
-                        Filter op level 1 en dan op een hoofdstuk. Zo kun je gestructureerd requirements verwerken.
-                        </li>
-                        <li>
-                        Ga voor een requirement na of dit al afgevangen is door het framework. Voorbeeld behorend bij chapter Authentication, zoek op Google naar: 'identity framework password requirements'.
-                        </li>
-                    </ol>
+            </header>
+            <div className="container">
+                <h1>ASVS for Dummies <small>(ASVS 4.0)</small></h1>
+                <p><strong>tips:</strong></p>
+                <ol>
+                    <li>
+                    Gebruik de <a rel="noreferrer" target="_blank" href="https://owasp.org/www-project-secure-coding-practices-quick-reference-guide/">
+                    quick reference guide</a>, daar wordt per requirement zoveel mogelijk naar verwezen.
+                    </li>
+                    <li>
+                    Filter op level 1 en dan op een hoofdstuk. Zo kun je gestructureerd requirements verwerken.
+                    </li>
+                    <li>
+                    Ga voor een requirement na of dit al afgevangen is door het framework. Voorbeeld behorend bij chapter Authentication, zoek op Google naar: 'identity framework password requirements'.
+                    </li>
+                </ol>
 
-                    <ASVSListFilter
-                                    chapters={this.state.chapters}
-                                    setChapterCheck={(c: string) => this.setChapterCheck(c)}
-                                    setLevelCheck={(c: string) => this.setLevelCheck(c)}/>
-                    <ASVSList items={this.state.asvsItems}></ASVSList>
+                <ASVSListFilter
+                                chapters={chapters}
+                                setChapterCheck={(c: string) => setChapterCheck(c)}
+                                setLevelCheck={(c: string) => setLevelCheck(c)}/>
+                <ASVSList items={asvsItems}></ASVSList>
 
-                </div>
             </div>
-        );
-    }
-
+        </div>
+    );
 }
 
 export default App;
