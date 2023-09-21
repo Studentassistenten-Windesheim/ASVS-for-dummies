c4c54b4a91e441096747966a4b3a96811986a308
jaelk <jaelkwakkel@gmail.com>
2023-09-21 13:56:09 +0200

    Added more tests

---
+++ cypress/e2e/asvsfilteringtests.cy.js
@@ -0,0 +1,34 @@
+// For autocompletion
+/// <reference types="Cypress" />
+
+describe("Filter items", () => {
+    beforeEach(() => {
+        cy.visit("http://localhost:5173");
+    });
+
+    it("should load the ASVS app", () => {
+        cy.contains("ASVS for Dummies (ASVS 4.0)");
+    });
+
+    it("should filter ASVS items by chapter", () => {
+        cy.get("[data-cy=chapter-checkbox]").eq(0).check();
+        // Check if all items contain the text: "Architecture, Design and Threat Modeling"
+        cy.get("[data-cy=asvs-list-item].true").should('contain.text', 'Architecture, Design and Threat Modeling');
+    });
+
+    it("should filter ASVS items by level", () => {
+        cy.get("[data-cy=level-checkbox]").eq(0).check();
+        cy.get("[data-cy=asvs-list-item].true").should("have.length.gt", 0);
+    });
+
+    it("should show ASVS items when a chapter is clicked", () => {
+        cy.get("[data-cy=chapter-checkbox]").first().check();
+        cy.get("[data-cy=asvs-list-item].true").should("have.length.gt", 0);
+    });
+
+    it("should show all ASVS items when no chapter is clicked", () => {
+        cy.get("[data-cy=chapter-checkbox]").first().check();
+        cy.get("[data-cy=chapter-checkbox]").first().uncheck();
+        cy.get("[data-cy=asvs-list-item].true").should("have.length.gt", 0);
+    });
+});
--- cypress/e2e/routingtests.cy.js
+++ cypress/e2e/routingtests.cy.js
@@ -1,23 +1,26 @@
+// For autocompletion
+/// <reference types="Cypress" />
+
 describe('Visit tips page', () => {
     it('clicks the button "Tips"', () => {
-        cy.visit("localhost:5173")
+        cy.visit("localhost:5173");
 
-        cy.contains('Tips').click()
+        cy.contains('Tips').click();
 
         // Should be on a new URL which
         // includes '/tips'
-        cy.url().should('include', '/tips')
-    })
-})
+        cy.url().should('include', '/tips');
+    });
+});
 
 describe('Visit home page', () => {
     it('clicks the button "Home"', () => {
-        cy.visit("localhost:5173/tips")
+        cy.visit("localhost:5173/tips");
 
-        cy.contains('Home').click()
+        cy.contains('Home').click();
 
         // Should be on a new URL which
         // includes '/'
-        cy.url().should('include', '/')
-    })
-})
+        cy.url().should('include', '/');
+    });
+});
