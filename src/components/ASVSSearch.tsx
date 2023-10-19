ba3a0eacd7bec0badc2bb7b247fbd84365db6bc4
Tycho <s1095019@student.windeshei.nl>
2023-10-19 11:10:26 +0200

    Added focus to search input on /

--- src/components/ASVSSearch.tsx
+++ src/components/ASVSSearch.tsx
@@ -1,22 +1,38 @@
 import ASVSItem from '../model/ASVSItem';
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 
 type Props = {
   setSearchInputCheck: (searchInput: string) => void;
 };
 
 const ASVSSearch: React.FC<Props> = ({ setSearchInputCheck }) => {
+  const searchInputRef = useRef<HTMLInputElement>(null);
+
   const handleChange = (searchInput: any) => {
     setSearchInputCheck(searchInput.target.value);
   };
+
+  useEffect(() => {
+    const keyUpHandler = (event: any) => {
+      if (event.key == '/' && searchInputRef.current) {
+        searchInputRef.current.focus();
+      }
+    };
+    document.addEventListener('keyup', keyUpHandler);
+
+    return () => {
+      document.removeEventListener('keyup', keyUpHandler);
+    };
+  }, []);
 
   return (
     <>
       <div>
         <input
+          ref={searchInputRef}
           className='w-50% sm:w-48 h-8 p-2 mt-8 ml-4 mb-1 border rounded-lg mb-4 focus:outline-none focus:border-gray-500 focus:ring-gray-200 hover:border-gray-300 shadow-sm'
           type='text'
-          placeholder='Search here'
+          placeholder='Press / to search'
           onChange={handleChange}
           data-cy='search-input'
         />
