fb9c4ff4b02f42696bad0ead52e2b6f804985e8e
Tycho <s1095019@student.windeshei.nl>
2023-10-19 13:47:14 +0200

    Added tests for search bar

--- src/components/ASVSSearch.tsx
+++ src/components/ASVSSearch.tsx
@@ -29,6 +29,7 @@ const ASVSSearch: React.FC<Props> = ({ setSearchInputCheck }) => {
     <>
       <div>
         <input
+          id='search-bar'
           ref={searchInputRef}
           className='w-50% sm:w-48 h-8 p-2 mt-8 ml-4 mb-1 border rounded-lg mb-4 focus:outline-none focus:border-gray-500 focus:ring-gray-200 hover:border-gray-300 shadow-sm'
           type='text'
