692b8ae2811ebf165ff07fce01ca33d14435e67a
jaelk <jaelkwakkel@gmail.com>
2023-11-16 19:48:00 +0100

    Solve rebase conflicts

--- src/components/ASVSList.tsx
+++ src/components/ASVSList.tsx
@@ -14,39 +14,6 @@ type Props = {
     setPinStatus: (itemId: string) => void;
 };
 
-// Detect links in text and make them clickable
-const DetectLink = ({ children }: any) => {
-    const extractUrl = (word: string) => {
-        const urlPattern = /\((https?:\/\/\S+?)\)/g; // Regex pattern to find links
-        const match = urlPattern.exec(word);
-        return match ? match[1] : null;
-    };
-
-    const url = extractUrl(children);
-
-    if (url) {
-        // If a URL is found, split the children into parts (text before URL and URL)
-        const parts = children.split(url);
-
-        return (
-            <span>
-                {parts[0]} {/* Render text before the URL */}
-                <a
-                    href={url}
-                    target='_blank'
-                    rel='noreferrer'
-                    className='font-medium text-blue-600 hover:underline'
-                >
-                    {url}
-                </a>
-                {parts[1]} {/* Render text after the URL */}
-            </span>
-        );
-    } else {
-        return <span>{children}</span>;
-    }
-};
-
 const ASVSList: React.FC<Props> = ({
     items,
     pinnedItems,
