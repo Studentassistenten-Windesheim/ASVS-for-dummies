7054579dfff94b2ee49cafc7a9a361b210d1e7e1
jaelkwakkel <62651445+jaelkwakkel@users.noreply.github.com>
2024-03-20 11:40:37 +0100

    Show chapter ID for easier filtering

--- src/components/ASVSChapterFilterItem.tsx
+++ src/components/ASVSChapterFilterItem.tsx
@@ -1,30 +1,42 @@
-import React from 'react';
+import React from "react";
 
 type Props = {
-    name: string,
-    checked: boolean,
-    toggleFunction: (name: string) => void,
-}
+  chapterId: number;
+  name: string;
+  checked: boolean;
+  toggleFunction: (name: string) => void;
+};
 
-const ASVSChapterFilterItem: React.FC<Props> = ({ name, checked, toggleFunction }) => {
-    return (
-        <li>
-            <div className='pl-3'>
-                <input
-                    type='checkbox'
-                    id={name}
-                    className='text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2'
-                    name={name}
-                    data-cy='chapter-checkbox'
-                    checked={checked}
-                    onChange={(e) => {
-                        toggleFunction(name);
-                    }}
-                />
-                <label htmlFor={name} className='w-full py-1 ml-2 text-sm text-gray-900'>{name}</label>
-            </div>
-        </li>
-    );
-}
+const ASVSChapterFilterItem: React.FC<Props> = ({
+  chapterId,
+  name,
+  checked,
+  toggleFunction,
+}) => {
+  return (
+    <li>
+      <div className="pl-3">
+        <input
+          type="checkbox"
+          id={name}
+          className="text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2"
+          name={name}
+          data-cy="chapter-checkbox"
+          checked={checked}
+          onChange={(e) => {
+            toggleFunction(name);
+          }}
+        />
+        <label
+          htmlFor={name}
+          className="w-full py-1 ml-2 text-sm text-gray-900"
+        >
+          <strong>{"V" + chapterId + ": "}</strong>
+          {name}
+        </label>
+      </div>
+    </li>
+  );
+};
 
 export default ASVSChapterFilterItem;
